92905e985f2f592fc2df030f32a7da5756691451
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-25 02:51:03 +0300

    Question01

--- OOP Assignment5/Program.cs
+++ OOP Assignment5/Program.cs
@@ -4,10 +4,31 @@
     {
         static void Main(string[] args)
         {
-           
 
 
 
+            #region Question01
+
+            /// 01) Object Copying
+            ///
+            /// a) What happens when you assign one object variable to another object variable?
+            ///
+            /// When one object variable is assigned to another object variable,
+            /// both variables refer to the same object in memory.
+            ///
+            /// b) Does assigning one object to another create a new object? Explain.
+            ///
+            /// No, assigning one object variable to another does not create a new object.
+            /// It only copies the reference to the same object.
+            ///
+            /// c) What is the difference between copying an object and copying its reference?
+            ///
+            /// Copying a reference means that two variables refer to the same object.
+            /// Copying an object means creating a new object with its own data.
+            ///
+          
+
+            #endregion
 
 
 
