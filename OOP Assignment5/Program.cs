bdb8aa9de1d754519024e6933fc3e2ccc114d70b
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-25 02:47:13 +0300

    Add project files.

---
+++ OOP Assignment5/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace OOP_Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
