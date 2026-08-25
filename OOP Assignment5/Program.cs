5f3e98c6f47c678d7294d29f20b43ec10fb06b14
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-25 03:13:49 +0300

    7. Interfaces

--- OOP Assignment5/Program.cs
+++ OOP Assignment5/Program.cs
@@ -525,108 +525,120 @@
 
             #region InternationalShipment
 
-        //public class InternationalShipment :
-        //    Shipment,
-        //    ITrackable,
-        //    IInsurable
-        //        {
-        //            public string DestinationCountry
-        //            {
-        //                get;
-        //                set;
-        //            }
+            //public class InternationalShipment :
+            //    Shipment,
+            //    ITrackable,
+            //    IInsurable
+            //        {
+            //            public string DestinationCountry
+            //            {
+            //                get;
+            //                set;
+            //            }
 
-        //            public decimal CustomsFee
-        //            {
-        //                get;
-        //                set;
-        //            }
+            //            public decimal CustomsFee
+            //            {
+            //                get;
+            //                set;
+            //            }
 
-        //            public InternationalShipment(
-        //                string trackingCode,
-        //                string description,
-        //                decimal weight,
-        //                decimal deliveryFee,
-        //                DeliveryAddress destination,
-        //                string destinationCountry,
-        //                decimal customsFee)
-        //                : base(
-        //                    trackingCode,
-        //                    description,
-        //                    weight,
-        //                    deliveryFee,
-        //                    destination)
-        //            {
-        //                DestinationCountry =
-        //                    destinationCountry;
+            //            public InternationalShipment(
+            //                string trackingCode,
+            //                string description,
+            //                decimal weight,
+            //                decimal deliveryFee,
+            //                DeliveryAddress destination,
+            //                string destinationCountry,
+            //                decimal customsFee)
+            //                : base(
+            //                    trackingCode,
+            //                    description,
+            //                    weight,
+            //                    deliveryFee,
+            //                    destination)
+            //            {
+            //                DestinationCountry =
+            //                    destinationCountry;
 
-        //                CustomsFee = customsFee;
-        //            }
+            //                CustomsFee = customsFee;
+            //            }
 
-        //            public override decimal EstimatedCost
-        //            {
-        //                get
-        //                {
-        //                    return DeliveryFee +
-        //                           (Weight * 5) +
-        //                           CustomsFee;
-        //                }
-        //            }
+            //            public override decimal EstimatedCost
+            //            {
+            //                get
+            //                {
+            //                    return DeliveryFee +
+            //                           (Weight * 5) +
+            //                           CustomsFee;
+            //                }
+            //            }
 
-        //            public override void PrintShipment()
-        //            {
-        //                Console.WriteLine(
-        //                    "International Shipment");
+            //            public override void PrintShipment()
+            //            {
+            //                Console.WriteLine(
+            //                    "International Shipment");
 
-        //                Console.WriteLine();
+            //                Console.WriteLine();
 
-        //                Console.WriteLine(
-        //                    "Tracking Code       : "
-        //                    + TrackingCode);
+            //                Console.WriteLine(
+            //                    "Tracking Code       : "
+            //                    + TrackingCode);
 
-        //                Console.WriteLine(
-        //                    "Description         : "
-        //                    + Description);
+            //                Console.WriteLine(
+            //                    "Description         : "
+            //                    + Description);
 
-        //                Console.WriteLine(
-        //                    "Weight              : "
-        //                    + Weight + " KG");
+            //                Console.WriteLine(
+            //                    "Weight              : "
+            //                    + Weight + " KG");
 
-        //                Console.WriteLine(
-        //                    "Delivery Fee        : "
-        //                    + DeliveryFee + " EGP");
+            //                Console.WriteLine(
+            //                    "Delivery Fee        : "
+            //                    + DeliveryFee + " EGP");
 
-        //                Console.WriteLine(
-        //                    "Destination Country : "
-        //                    + DestinationCountry);
+            //                Console.WriteLine(
+            //                    "Destination Country : "
+            //                    + DestinationCountry);
 
-        //                Console.WriteLine(
-        //                    "Customs Fee         : "
-        //                    + CustomsFee + " EGP");
+            //                Console.WriteLine(
+            //                    "Customs Fee         : "
+            //                    + CustomsFee + " EGP");
 
-        //                Console.WriteLine(
-        //                    "Estimated Cost      : "
-        //                    + EstimatedCost + " EGP");
-        //            }
+            //                Console.WriteLine(
+            //                    "Estimated Cost      : "
+            //                    + EstimatedCost + " EGP");
+            //            }
 
-        //            string ITrackable.GetTrackingStatus()
-        //            {
-        //                return "Shipment "
-        //                    + TrackingCode
-        //                    + " has been Delivered.";
-        //            }
+            //            string ITrackable.GetTrackingStatus()
+            //            {
+            //                return "Shipment "
+            //                    + TrackingCode
+            //                    + " has been Delivered.";
+            //            }
 
-        //            public decimal CalculateInsurance()
-        //            {
-        //                return EstimatedCost * 0.12m;
-        //            }
-        //        }
+            //            public decimal CalculateInsurance()
+            //            {
+            //                return EstimatedCost * 0.12m;
+            //            }
+            //        }
 
             #endregion
 
 
 
+            #region Interfaces
 
+        //public interface ITrackable
+        //        {
+        //            string GetTrackingStatus();
+        //        }
+
+        //        public interface IInsurable
+        //        {
+        //            decimal CalculateInsurance();
+        //        }
+
+            #endregion
 
 
 
