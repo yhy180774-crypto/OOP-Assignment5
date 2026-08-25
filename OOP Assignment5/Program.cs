84d7521c7185953804077a10e42d9e5ca9271087
iTech <iTech@DESKTOP-RT7KEOQ>
2026-08-25 03:09:57 +0300

    4 .StandardShipment

--- OOP Assignment5/Program.cs
+++ OOP Assignment5/Program.cs
@@ -359,7 +359,82 @@
 
 
 
+            #region StandardShipment
 
+        //public class StandardShipment :
+        //    Shipment,
+        //    ITrackable,
+        //    IInsurable
+        //        {
+        //            public StandardShipment(
+        //                string trackingCode,
+        //                string description,
+        //                decimal weight,
+        //                decimal deliveryFee,
+        //                DeliveryAddress destination)
+        //                : base(
+        //                    trackingCode,
+        //                    description,
+        //                    weight,
+        //                    deliveryFee,
+        //                    destination)
+        //            {
+        //            }
+
+        //            public override decimal EstimatedCost
+        //            {
+        //                get
+        //                {
+        //                    return DeliveryFee +
+        //                           (Weight * 5);
+        //                }
+        //            }
+
+        //            public override void PrintShipment()
+        //            {
+        //                Console.WriteLine(
+        //                    "Standard Shipment");
+
+        //                Console.WriteLine();
+
+        //                Console.WriteLine(
+        //                    "Tracking Code : "
+        //                    + TrackingCode);
+
+        //                Console.WriteLine(
+        //                    "Description   : "
+        //                    + Description);
+
+        //                Console.WriteLine(
+        //                    "Weight        : "
+        //                    + Weight + " KG");
+
+        //                Console.WriteLine(
+        //                    "Delivery Fee  : "
+        //                    + DeliveryFee + " EGP");
+
+        //                Console.WriteLine(
+        //                    "Estimated Cost: "
+        //                    + EstimatedCost + " EGP");
+        //            }
+
+        //            public string GetTrackingStatus()
+        //            {
+        //                return GetTrackingStatusFromShipment();
+        //            }
+
+        //            private string GetTrackingStatusFromShipment()
+        //            {
+        //                return GetTrackingStatus();
+        //            }
+
+        //            public decimal CalculateInsurance()
+        //            {
+        //                return EstimatedCost * 0.05m;
+        //            }
+        //        }
+
+            #endregion
 
 
 
